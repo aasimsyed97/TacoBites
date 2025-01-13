1e8199ad891a594515f14e5c6f0501f9eb1b1bf3
aasimsyed97 <sayedaasim555@gmail.com>
2025-01-13 23:08:54 +0530

    taco jpg, taco bites application file added.

---
+++ TacoBites/src/main/java/com/TacoBites/TacoBitesApplication.java
@@ -0,0 +1,19 @@
+package com.TacoBites;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TacoBitesApplication {
+
+
+
+	public static void main(String[] args) {
+		SpringApplication.run(TacoBitesApplication.class, args);
+
+
+
+	}
+
+
+}
---
+++ TacoBites/src/test/java/com/TacoBites/TacoBitesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.TacoBites;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TacoBitesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
