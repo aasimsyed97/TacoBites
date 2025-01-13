1f1d3a6d3df4418677c439b3946f29e094e905ca
aasimsyed97 <sayedaasim555@gmail.com>
2025-01-13 23:00:01 +0530

    Order model and page updated

--- TacoBites/src/main/java/com/TacoBites/controller/DesignTacoController.java
+++ TacoBites/src/main/java/com/TacoBites/controller/DesignTacoController.java
@@ -7,10 +7,12 @@ import com.TacoBites.model.Type;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import javax.validation.Valid;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -44,13 +46,16 @@ public class DesignTacoController {
         return "design";
     }
 
-//    @PostMapping
-//    public String processDesign(Design design) {
-//        // Save the taco design...
-//        // We'll do this in chapter 3
-//        log.info("Processing design: " + design);
-//        return "redirect:/orders/current";
-//    }
+    @PostMapping
+    public String processDesign(@Valid Taco design, Errors errors) {
+        if (errors.hasErrors()) {
+            return "design";
+        }
+        // Save the taco design...
+        // We'll do this in chapter 3
+        log.info("Processing design: " + design);
+        return "redirect:/orders/current";
+    }
     private List<Ingredient> filterByType(List<Ingredient> ingredients , Type type ){
         // logic for filteration;
        return ingredients.stream().filter(ingredient -> ingredient.getType().equals(type)).collect(Collectors.toList());
