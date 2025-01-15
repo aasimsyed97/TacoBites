1b5a57447c3fa7b6249e5bdde470d801f90e5613
aasimsyed97 <sayedaasim555@gmail.com>
2025-01-15 23:16:55 +0530

    jdbc ingredient repo added.

--- TacoBites/src/main/java/com/TacoBites/repository/JDBCIngredientRepository.java
+++ TacoBites/src/main/java/com/TacoBites/repository/JDBCIngredientRepository.java
@@ -1,8 +1,13 @@
 package com.TacoBites.repository;
 
 import com.TacoBites.model.Ingredient;
+import com.TacoBites.model.Type;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 
 public class JDBCIngredientRepository implements IngredientRepository{
@@ -15,16 +20,38 @@ public class JDBCIngredientRepository implements IngredientRepository{
     }
     @Override
     public Iterable<Ingredient> findAll() {
-        return null;
+        return jdbc.query("select id, name, type from Ingredient",
+                this::mapRowToIngredient);
     }
 
+
+//    alternate solution with rowmapper interface implementation.
+//    @Override
+//    public Ingredient findOne(String id) {
+//        return jdbc.queryForObject(
+//                "select id, name, type from Ingredient where id=?",
+//                new RowMapper<Ingredient>() {
+//                    public Ingredient mapRow(ResultSet rs, int rowNum)
+//                            throws SQLException {
+//                        return new Ingredient(
+//                                rs.getString("id"),
+//                                rs.getString("name"),
+//                               Type.valueOf(rs.getString("type")));
+//                    };
+//                }, id);
+//    }
     @Override
     public Ingredient findOne(String id) {
-        return null;
+        return jdbc.queryForObject(
+                "select id, name, type from Ingredient where id=?",
+                this::mapRowToIngredient, id);
     }
+
 
     @Override
     public Ingredient save(Ingredient ingredient) {
-        return null;
+        return null ;
     }
+
+
 }
