b5fd95ea0dd6e445ecc215e5c483438b93a36ca4
aasimsyed97 <sayedaasim555@gmail.com>
2025-01-18 21:56:34 +0530

    DesignTacoController updated

--- TacoBites/src/main/java/com/TacoBites/controller/DesignTacoController.java
+++ TacoBites/src/main/java/com/TacoBites/controller/DesignTacoController.java
@@ -2,17 +2,17 @@ package com.TacoBites.controller;
 
 
 import com.TacoBites.model.Ingredient;
+import com.TacoBites.model.Order;
 import com.TacoBites.model.Taco;
 import com.TacoBites.model.Type;
 import com.TacoBites.repository.IngredientRepository;
+import com.TacoBites.repository.TacoRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import java.util.ArrayList;
@@ -23,12 +23,27 @@ import java.util.stream.Collectors;
 @Slf4j
 @Controller
 @RequestMapping("/design")
+@SessionAttributes("order")
 public class DesignTacoController {
 
     private final IngredientRepository ingredientRepo;
+    private TacoRepository designRepo;
+
     @Autowired
-    public DesignTacoController(IngredientRepository ingredientRepo) {
+    public DesignTacoController(
+            IngredientRepository ingredientRepo,
+            TacoRepository designRepo) {
         this.ingredientRepo = ingredientRepo;
+        this.designRepo = designRepo;
+    }
+
+    @ModelAttribute(name = "order")
+    public Order order() {
+        return new Order();
+    }
+    @ModelAttribute(name = "taco")
+    public Taco taco() {
+        return new Taco();
     }
 
     @GetMapping
@@ -57,13 +72,14 @@ public class DesignTacoController {
     }
 
     @PostMapping
-    public String processDesign(@Valid Taco design, Errors errors) {
+    public String processDesign(
+            @Valid Taco design, Errors errors,
+            @ModelAttribute Order order) {
         if (errors.hasErrors()) {
             return "design";
         }
-        // Save the taco design...
-        // We'll do this in chapter 3
-        log.info("Processing design: " + design);
+        Taco saved = designRepo.save(design);
+        order.addDesign(saved);
         return "redirect:/orders/current";
     }
     private List<Ingredient> filterByType(List<Ingredient> ingredients , Type type ){
