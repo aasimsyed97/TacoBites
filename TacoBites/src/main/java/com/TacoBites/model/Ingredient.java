6d25d8bd53b0c1b462b534d5f4ebe8ecfcb1d5cc
aasimsyed97 <sayedaasim555@gmail.com>
2025-01-19 17:10:14 +0530

    updating models/domains as per the jpa

--- TacoBites/src/main/java/com/TacoBites/model/Ingredient.java
+++ TacoBites/src/main/java/com/TacoBites/model/Ingredient.java
@@ -1,11 +1,20 @@
 package com.TacoBites.model;
 
 
+import lombok.AccessLevel;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
 
 @Data
+@RequiredArgsConstructor
+@NoArgsConstructor(access= AccessLevel.PRIVATE, force=true)
+@Entity
 public class Ingredient {
-    private final String id;
+    @Id
     private final String name;
     private final Type type;
 
