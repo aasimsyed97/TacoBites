fd9e5f414995f880447fa152fab75190b3935c8f
aasimsyed97 <sayedaasim555@gmail.com>
2025-01-20 22:43:46 +0530

    spring security added to application

--- TacoBites/src/main/java/com/TacoBites/Config/Security/SecurityConfig.java
+++ TacoBites/src/main/java/com/TacoBites/Config/Security/SecurityConfig.java
@@ -1,10 +1,58 @@
 package com.TacoBites.Config.Security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.StandardPasswordEncoder;
+
+import javax.sql.DataSource;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    DataSource dataSource;
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception{
+        auth
+                // In memory authentication configuration
+//                .inMemoryAuthentication()
+//                .withUser("jack")
+//                .password("sparrow")
+//                .authorities("ROLE_USER")
+//                .and()
+//                .withUser("elon")
+//                .password("musk")
+//                .authorities("ROLE_USER");
+        // JDBC based authentication configuration
+//                .jdbcAuthentication()
+//                .dataSource(dataSource)
+//                .usersByUsernameQuery(
+//                        "select username, password, enabled from Users " +
+//                                "where username=?")
+//                .authoritiesByUsernameQuery(
+//                        "select username, authority from UserAuthorities " +
+//                                "where username=?")
+//                .passwordEncoder(new StandardPasswordEncoder("53cr3t"));
+
+        // LDAP based authentication configuration
+                .ldapAuthentication()
+                .userSearchBase("ou=people")
+                .userSearchFilter("(uid={0})")
+                .groupSearchBase("ou=groups")
+                .groupSearchFilter("member={0}")
+                .passwordCompare()
+                .passwordEncoder(new BCryptPasswordEncoder())
+                .passwordAttribute("passcode")
+                .and()
+                .contextSource()
+                .root("dc=tacocloud,dc=com");
+
+
+    }
 }
